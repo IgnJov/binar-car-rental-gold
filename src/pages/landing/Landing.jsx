927325d8e75a6a4d559e0c1aac819385aeb7f468
IgnJov <jovankaignatius@gmail.com>
2023-11-14 00:13:20 +0700

    Render landing page component

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,7 +1,27 @@
-import react from "react";
+import React, { useRef } from "react";
+import "./Landing.css";
+import Header from "../../components/header/Header.jsx";
+import Hero from "../../sections/hero/Hero.jsx";
+import OurServices from "../../sections/our-services/OurServices.jsx";
+import WhyUs from "../../sections/why-us/WhyUs.jsx";
+import Testimonial from "../../sections/testimonial/Testimonial.jsx";
+import CTABanner from "../../sections/cta-banner/CTABanner.jsx";
+import FAQ from "../../sections/faq/FAQ.jsx";
+import Footer from "../../components/footer/Footer.jsx";
 
 function Landing() {
-    return <div></div>;
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <OurServices />
+            <WhyUs />
+            <Testimonial />
+            <CTABanner />
+            <FAQ />
+            <Footer />
+        </div>
+    );
 }
 
 export default Landing;
