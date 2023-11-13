cad2f655da2f83484df9b0325913cc54273a9ea6
IgnJov <jovankaignatius@gmail.com>
2023-11-13 23:54:37 +0700

    Route landing page to browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Landing from "./pages/landing/Landing.jsx";
+import Filter from "./pages/search/Search.jsx";
+import Detail from "./pages/detail/Detail.jsx";
+import Error from "./pages/error/Error.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-    return <div></div>;
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Landing />,
+            errorElement: <Error />,
+        },
+        {
+            path: "/search",
+            element: <Filter />,
+        },
+        {
+            path: "/detail/:id",
+            element: <Detail />,
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
 }
 
 export default App;
